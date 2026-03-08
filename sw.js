e1a2653b3bd45527b4623f766be1bb6fa4cf50b7
junjuuun1974-max <jun.juuun1974@gmail.com>
2026-03-09 08:11:30 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
- * sw.js - Uni献立ナビ v4 Service Worker
+ * sw.js - Uni献立ナビ v3 Service Worker
  */
-var CACHE_VERSION = 'v4';
+var CACHE_VERSION = 'v5';
 var CACHE_NAME = 'uni-kondate-navi-' + CACHE_VERSION;
 var PRECACHE_URLS = [
   './index.html',
